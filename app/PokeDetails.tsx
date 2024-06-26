a9bf25a04b0c5e88e53ab625a918d1bceab30b5f
Nicolas <nicolasgp.ec@gmail.com>
2024-06-26 18:55:05 -0300

    Fix PokeName on Pokemon Modal

--- app/PokeDetails.tsx
+++ app/PokeDetails.tsx
@@ -12,7 +12,7 @@ export default function PokeDetails({pokeColors,pokemon,onClose,open,selectedPok
                     onClose={onClose}>
                     <Box sx={{width:'40%',height:'70%',boxShadow:20, backgroundColor:pokeColors[pokemon.types[0].type.name].card,top:'15vh',left:'30vw',transform:'translate(30vw,15vh)',borderRadius:'20px',textAlign:'center',outline:'none'}}>
                         <Image src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} width={200} height={200} style={{marginTop:'20px', left:'50%'}}/>
-                        <p>{pokemon.name}</p>
+                        <p>${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}</p>
                         <p>Experiência básica: {pokemon.base_experience}</p>
                         <p>{pokemon.base_experience}</p>
                         <p>{pokemon.weight}</p>
